7131805037a9051744c9c0fe55d175f0fa40b589
c <imcling@qq.com>
2021-05-22 16:04:48 +0800

    login

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,13 +1,44 @@
 <template>
-    <div>login</div>
+    <div>
+        <el-card class="box-card">
+            <template #header>
+                <span>用户登录</span>
+            </template>
+            <el-form ref="login" :model="login">
+                <el-form-item label="账号">
+                    <el-input v-model="login.username" aria-placeholder="请输入账号"></el-input>
+                </el-form-item>
+                <el-form-item label="密码">
+                    <el-input v-model="login.password" type="password" aria-placeholder="请输入密码"></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="onLogin">登录</el-button>
+                    <el-button type="primary">重置</el-button>
+                </el-form-item>
+            </el-form>
+        </el-card>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "login"
+        name: "login",
+        data() {
+            return {
+                login: {}
+
+            }
+        },
+        methods: {
+            onLogin() {
+
+            },
+        }
     }
 </script>
 
 <style scoped>
-
+    .box-card {
+        width: 300px;
+    }
 </style>
