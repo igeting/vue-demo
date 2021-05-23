a83876ef0edaa281239bc592c44ea8487e682f61
c <imcling@qq.com>
2021-05-23 21:06:08 +0800

    app

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from '@/app.vue'
+import app from '@/app.vue'
 import router from '@/router'
 import store from '@/store'
 import ElementUI from 'element-ui'
@@ -12,5 +12,5 @@ Vue.config.productionTip = false
 new Vue({
     router,
     store,
-    render: h => h(App)
+    render: h => h(app)
 }).$mount('#app')
